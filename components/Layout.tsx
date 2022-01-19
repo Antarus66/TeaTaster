6bd72ea0995f5f4ee3d6d61e70504a730d086de8
Andrii Tarusin <antarus66@gmail.com>
2022-01-19 17:30:43 +0200

    Changed files structure of the header-used components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from '@mui/material';
-import Header from './Header';
+import Header from './header/Header';
 
 export interface LayoutProps {
   example?: null
