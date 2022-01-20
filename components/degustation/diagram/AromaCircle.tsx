9b82c97ce770ba597b17ed988c623aab8955cdcd
Andrii Tarusin <antarus66@gmail.com>
2022-01-20 17:14:28 +0200

    Mage the diagramm to use parent node color. Added an loggingcliked leaf node name.

--- components/degustation/diagram/AromaCircle.tsx
+++ components/degustation/diagram/AromaCircle.tsx
@@ -1,37 +1,64 @@
 import React, { useEffect, useRef } from 'react';
-import Sunburst from 'sunburst-chart';
+import Sunburst, { Node } from 'sunburst-chart';
+import * as d3 from 'd3';
+import { RGBColor } from 'd3-color';
 import data from './d3-data.json';
 import styles from './AromaCircle.module.css';
 
-const AromaCircle: React.FC = () => {
+interface AromaCircleProps {
+  width: number
+}
+
+const AromaCircle: React.FC<AromaCircleProps> = ({ width }) => {
   const chartRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const myChart = Sunburst();
-    const width = 900;
-
     if (chartRef.current !== null) {
-      myChart.data(data)
-        .excludeRoot(true)
-        .radiusScaleExponent(1.7)
-        .labelOrientation('radial')
-        .color((d) => {
-          if (d.color) {
-            return d.color;
-          }
-
-          return 'lightgrey';
-        })
-        .width(width) // todo: move out to props
-        .height(width)(chartRef.current);
+      createChart(chartRef.current, width);
     }
   }, []);
 
-  const onSelect = (e: any) => {
-    console.log(e);
-  };
-
   return <div className={styles.chartAnchor} ref={chartRef}/>;
 };
+
+function createChart(anchor: HTMLDivElement, width: number) {
+  const myChart = Sunburst();
+
+  myChart.data(data)
+    .excludeRoot(true)
+    .radiusScaleExponent(1.7)
+    .labelOrientation('radial')
+    .color(findNodeColor)
+    .onClick((node: Node) => {
+      if (!node) {
+        return;
+      }
+
+      if (node.children?.length) {
+        myChart.focusOnNode(node);
+        return;
+      }
+
+      console.log(node.name);
+    })
+    .width(width)
+    .height(width)(anchor);
+}
+
+function findNodeColor(node: Node) {
+  if (node.color) {
+    return node.color;
+  }
+
+  if (node.__dataNode?.parent) {
+    const parentColor: string = findNodeColor(node.__dataNode.parent.data);
+    const d3ParentColor = d3.color(parentColor) as RGBColor;
+    d3ParentColor.opacity = 0.6;
+
+    return d3ParentColor.toString();
+  }
+
+  return 'lightgrey';
+}
 
 export default AromaCircle;
--- pages/new-degustation.tsx
+++ pages/new-degustation.tsx
@@ -1,16 +1,10 @@
 import { NextPage } from 'next';
-import { useTranslation } from 'react-i18next';
-// import AromaCircle from '../components/degustation/diagram/AromaCircle';
 import dynamic from 'next/dynamic';
 
 const AromaCircle = dynamic(() => import('../components/degustation/diagram/AromaCircle'), { ssr: false });
 
-const NewDegustation: NextPage = () => {
-  const { t } = useTranslation();
-
-  return (
-    <AromaCircle/>
-  );
-};
+const NewDegustation: NextPage = () => (
+    <AromaCircle width={900}/>
+);
 
 export default NewDegustation;
